35bf32eb09868b151d85b914ab23144f6e9ccf25
onlywillian <willianandrade953@gmail.com>
2022-07-05 23:17:17 -0300

    Adicionando fetch

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,29 @@
 import { useState } from 'react';
-import { SafeAreaView, StyleSheet, TextInput, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, TextInput, Button } from 'react-native';
+import axios from 'axios';
 
 import loginImage from '../assets/screen/login.png';
 
 export default function InvenctoryScreen() {
     const [ value, setValue ] = useState('');
 
+    const handleButtonClick = () => {
+        fetch('https://mywebsite.com/endpoint/', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: value,
+            })
+        });
+    }
+
     return (
         <SafeAreaView>
             <TextInput value={value} onChangeText={setValue} maxLength={20} style={styles.input}/>
+            <Button title='Go'onPress={handleButtonClick} />
         </SafeAreaView>
     )
 }
