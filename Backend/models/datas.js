f148bb1aac592b2e5238f001f0590e73c96f0950
Gabrielmarquezin <gabrielmarquesaraujs@gmail.com>
2022-07-07 22:22:47 -0300

    Sexta 22:22 07-07-22

--- Backend/models/datas.js
+++ Backend/models/datas.js
@@ -1,47 +1,6 @@
 const bd = require('../conection/conection.js')
 const db = bd.collection("Usuarios")
 
-const data = {
-    nome: '',
-    money: null,
-    roupas: {
-        calca: {
-            nome: '',
-            money: null
-        },
-        chapeu: {
-            nome: '',
-            money: null
-        },
-        tenis: {
-            nome: '',
-            money: null
-        },
-        blusa: {
-            nome: '',
-            money: null
-        }
-    },
-    respostas: {
-        item: '',
-        money: null
-    }
-}
-
-
-<<<<<<< HEAD:Backend/models/model.js
-
-const name = "Gabriel"
-const User = (async function(){
-   const nomes = await db.where('nome', '==', name).get()
-
-   if(nomes.empty){
-       const uss = await db.add(data)
-       const up = db.doc(`${uss.id}`).update({nome: name})
-       
-   }else{
-      console.log("DOCUMENT HAVE BEEN EXISTS")
-   }
 
 module.exports = db
 
--- Backend/models/server.js
+++ Backend/models/server.js
@@ -43,7 +43,7 @@ app.get("/", (req, res)=>{
         res.send(doc.data())
     })
    })
-
+   
 })
 
 app.post('/usuarios', (req, res)=>{
