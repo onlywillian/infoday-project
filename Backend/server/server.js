b9a5d4a4f860c2ae794ef8f0507b7d909dcfbcc2
Gabrielmarquezin <gabrielmarquesaraujs@gmail.com>
2022-07-19 17:06:58 -0300

    otimização 19/07/22

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -12,14 +12,14 @@ app.use(cors())
 // ------------- VALIDAÇÃO DE USUARIOS (CREATE)-------------------------
 app.post('/usuarios', async (req, res)=>{
     const nome = req.body.name    
+    data.nome = nome
+
     if(nome){
         const ValidName = await db.where("nome", "==", nome).get()
-
         if(ValidName.empty){
             const uss = await db.add(data)
-            const up = db.doc(`${uss.id}`).update({nome: nome})
             console.log("Usuario criado")
-            
+
         }else{
             res.send("Documento ja existe")
             console.log("DOCUMENTO JA EXISTE")
@@ -29,28 +29,18 @@ app.post('/usuarios', async (req, res)=>{
 
 /* ------------------------    (GET)  API DOS USUARIOS    -------------------------- */
 app.get('/usuarios/:user', async (req, res)=>{
-//-------------- example api --------------------
     const nome = req.params.user
-    if(nome == "Exemplo"){
-        const example = await db.where("nome", "==", "Exemplo").get()
-        example.forEach(doc => {
-            res.send(doc.data())
-        })
-        return;
-    }
-
-//------------- USER API -----------------
     const search = await db.where("nome", "==", nome).get()
+
     if(!search.empty){    
         search.forEach(doc => {
             res.send(doc.data())
-        })  
+        })       
     }else{
         res.send("Documento inxistente")
     }
-
+    
 })
-
 
 app.listen(3001, ()=>{
     console.log("CONEXAO ESTABELECIDA")
