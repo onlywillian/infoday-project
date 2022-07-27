87ebd6945239e627e8e734704b95e70f307b359a
Gabrielmarquezin <gabrielmarquesaraujs@gmail.com>
2022-07-27 17:59:05 -0300

    27/07/22 cru

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -10,21 +10,20 @@ app.use(express.urlencoded({extended: false}))
 app.use(cors())
 
 // ------------- VALIDAÇÃO DE USUARIOS (CREATE)-------------------------
-app.post('/usuarios', async (req, res)=>{
+app.post('/usuarios',  async (req, res)=>{
     const nome = req.body.name
     data.nome = nome
-
-    if(nome){
+    
         const ValidName = await db.where("nome", "==",nome).get()
-        if(ValidName.empty){
-            const uss = await db.add(data)
-            console.log("Usuario criado")
+        if(ValidName.empty){   
+                const uss = await db.add(data)
+                console.log("Usuario criado")  
 
         }else{
             res.send("Documento ja existe")
             console.log("DOCUMENTO JA EXISTE")
         }
-    }  
+
 })
 
 /* ------------------------    (GET)  API DOS USUARIOS    -------------------------- */
@@ -40,6 +39,30 @@ app.get('/usuarios/:user', async (req, res)=>{
         res.send("Documento inxistente")
     }
     
+})
+
+//-------------Atualizações dos Usuarios-----------------
+app.put('/usuarios/update/:idUser', async (req, res)=>{
+   const nome = req.params.idUser
+   const NewData = req.body
+
+   try {
+        const searchUser = await db.where('nome','==',nome).get()
+        searchUser.forEach((doc)=>{
+            const id = doc.id
+            db.doc(`${id}`).update(NewData)
+        })
+
+        if(!searchUser.empty){
+            res.send("Documento atualizado")
+        }else{
+            res.send("Documento nao existe")
+        }
+        
+   } catch (error) {
+        console.log("Usuario nao atualizado "+error)
+   }
+
 })
 
 app.listen(3001, ()=>{
