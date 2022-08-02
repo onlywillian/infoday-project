9458f7840877429b1d2b8cc6a2c87d7a0d06d8a7
Gabrielmarquezin <gabrielmarquesaraujs@gmail.com>
2022-08-01 22:33:34 -0300

    new 01/08/22

--- Backend/server/database/dataQuiz.js
+++ Backend/server/database/dataQuiz.js
@@ -1,106 +1,115 @@
 const { response } = require('express')
 const db = require('../../conection/conection.js')
 
-
 const QuestionRes = [
     {
-        question: "Qual seu nome?",
+        index: 1, 
+        question: "HTML é uma linguagem de: ",
         item: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        response: "a"
-    },
-    {
-    
-        question: "Qual sua idade?",
-        item: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        response: "b"
-    },
-    {
-        question: "Onde voce mora",
-        item: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        response: "c"
-    },
-    {
-        question: "O que voce faz?",
-        item: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
+            a: "Certificação",
+            b: "Programação",
+            c: "Desenvolvimento Web",
+            d: "Linguagem de marcação"
         },
         response: "d"
     },
     {
-        question: "O que voce tem?",
+        index: 2,
+        question: "CSS tem como principal função em um site:",
         item: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
+            a: "Estrutura",
+            b: "Dinâmica",
+            c: "Desenvolvimento Web",
+            d: "Estilização"
+        },
+        response: "d"
+    },
+    {
+        index: 3,
+        question: "A linguagem JAVASCRIPT pode ter várias funções dentro de um site, como:",
+        item: {
+            a: "Deixar sites dinâmicos",
+            b: "Estruturar sites",
+            c: "Estilizar sites",
+            d: "Estilizar botões"
+        },
+        response: "a",
+    },
+    {
+        index: 4,
+        question: "O inventor da apple se chama:",
+        item: {
+            a: "Bill Gates",
+            b: "Steve Jober",
+            c: "Mosely Gates",
+            d: "Steve Jobs"
+        },
+        response: "d",
+    },
+    {
+        index: 5,
+        question: "Qual o nome do invetor da Microsoft?",
+        item: {
+            a: "Bill Gates",
+            b: "Steve jobs",
+            c: "Mark zuckenberg",
+            d: "João Pedro Brito Sousa"
         },
         response: "a"
     },
     {
-        question: "O que voce quer?",
+        index: 6,
+        question: "Para que serve o cooler dentro de um computador?",
         item: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
+            a: "Evitar aquecimento das peças ",
+            b: "Fornecer energia ao computador ao girar",
+            c: "Processa dados rápidos ",
+            d: "Armazenar dados"
         },
-        response: "b"
+        response: "a"
     },
     {
-        question: "Onde andas?",
+        index: 7,
+        question: "Qual dos seguintes nomes são navegadores web?",
         item: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
+            a: "Windows",
+            b: "Linux",
+            c: "Mozila Firefox",
+            d: "Linux Mint"
         },
         response: "c"
     },
     {
-        question: "Onde foi?",
+        index: 8,
+        question: "Qual dos seguintes nomes é um sistema operacional:",
         item: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        response: "a"
-    },
-    {
-        question: "Onde queres?",
-        item: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
+            a: "Google Chrome",
+            b: "Ubuntu",
+            c: "Brave",
+            d: "Visual Studio"
         },
         response: "b"
     },
+
     {
-        question: "O que gostaria?",
+        index: 9,
+        question: "Qual a função de um HD?",
         item: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
+            a: "Armazenar memórias RAM",
+            b: "Resfriar o computador",
+            c: "Processa dados rápidos",
+            d: "Armazenar dados"
+        },
+        response: "d"
+    },
+    {
+        index: 10,
+        question: "Você gostou do jogo?",
+        item: {
+            a: "SIM",
+            b: "NAO",
+            c: 'UM POUCO',
+            d: 'NEUTRO'
         },
         response: "a"
     }
