1b00f856b1de1e212a102ea816a592f036525942
Gabrielmarquezin <gabrielmarquesaraujs@gmail.com>
2022-08-01 22:45:54 -0300

    Complete day 01/08/22

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -16,11 +16,12 @@ app.use((req, res, next) => {
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
+    
     next();
 })
 
 //create
-app.use('*', routerUser)
+app.use('/', routerUser)
 
 //read
 app.use('/', routerUser)
