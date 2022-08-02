80047981093a41322b9d0a680dc214bb11d38faa
Gabrielmarquezin <gabrielmarquesaraujs@gmail.com>
2022-08-01 21:26:05 -0300

    new

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -9,10 +9,18 @@ const Router = require('./routers/quiz')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors())
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+})
 
 //create
-app.use('/', routerUser)
+app.use('*', routerUser)
 
 //read
 app.use('/', routerUser)
