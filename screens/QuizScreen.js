14f0661d64befe8f73d6c5967a6a7c8de1c5c2c9
onlywillian <willianandrade953@gmail.com>
2022-08-01 22:39:11 -0300

    Quiz Screen and backend communication

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -1,64 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 export default function MainScreen() {
   const [index, setIndex] = useState(0);
   const [respondido, setRespondido] = useState(false);
-  const [correto, setCorreto] = useState(false);
+  const arr = [0, 2, 3, 4, 5, 6, 7, 8, 9, 1];
 
-  const questoes = [
-    {
-      question: 'vc é gay?',
-      a: 'Sim',
-      b: 'Talvez',
-      c: 'nao',
-      d: 'certeza',
-      response: 'a'
-    },
-    {
-      question: 'vc é gay manezao?',
-      a: 'asdsad',
-      b: 'asdsdadassad',
-      c: 'asdssaad',
-      d: 'asdsaaaaaaad',
-      response: 'd'
-    }
-  ];
+  const [questoes, setQuetoes] = useState([]);
 
-  const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#fff',
-      },
-      enunciado: {
-        flex: 1,
-        borderWidth: 1,
-        alignItems: 'center'
-      },
-      item: {
-        flex: 1,
-        borderWidth: 1,
-      },
-      correct: {
-        flex: 1,
-        borderWidth: 1,
-        backgroundColor: 'green',
-      },
-      incorrect: {
-        flex: 1,
-        borderWidth: 1,
-        backgroundColor: 'red',
-      }
-  });
+  useEffect(async () => {
+    let response = await fetch('http://localhost:3001/quiz/question')
+    let userData = await response.json();
 
-  const [styleIfCorrect, setstyleIfCorrect] = useState(styles.incorrect);
+    console.log(userData);
+    return setQuetoes(userData);
+  }, []);
 
   const handleItemClick = itemDeResposta => {
-    if (questoes[index].response === itemDeResposta && !respondido) {
-      setRespondido(true);
-      setCorreto(true);
-      setstyleIfCorrect(styles.correct);
-    }
+    setRespondido(true);
 
     let newIndex;
 
@@ -92,21 +51,42 @@ export default function MainScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.enunciado}>
-        <Text>{questoes[index].question}</Text>
+        <Text>{questoes.length != 0 ? questoes[arr[index]].question : 'Enunciado'}</Text>
       </View>
 
-      <TouchableOpacity style={respondido ? styleIfCorrect : styles.item} onPress={() => handleItemClick("a")}>
-        <Text>{questoes[index].a}</Text>
+      <TouchableOpacity style={respondido && questoes[index].response == "a" ? styles.correct : styles.item} onPress={() => handleItemClick("a")}>
+        <Text>{questoes.length != 0 ? questoes[arr[index]].item.a : 'a'}</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={respondido ? styleIfCorrect : styles.item} onPress={() => handleItemClick("b")}>
-        <Text>{questoes[index].b}</Text>
+      <TouchableOpacity style={respondido && questoes[index].response == "b" ? styles.correct : styles.item} onPress={() => handleItemClick("b")}>
+        <Text>{questoes.length != 0 ? questoes[arr[index]].item.b : 'a'}</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={respondido ? styleIfCorrect : styles.item} onPress={() => handleItemClick("c")}>
-        <Text>{questoes[index].c}</Text>
+      <TouchableOpacity style={respondido && questoes[index].response == "c" ? styles.correct : styles.item} onPress={() => handleItemClick("c")}>
+        <Text>{questoes.length != 0 ? questoes[arr[index]].item.c : 'a'}</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={respondido ? styleIfCorrect : styles.item} onPress={() => handleItemClick("d")}>
-        <Text>{questoes[index].d}</Text>
+      <TouchableOpacity style={respondido && questoes[index].response == "d" ? styles.correct : styles.item} onPress={() => handleItemClick("d")}>
+        <Text>{questoes.length != 0 ? questoes[arr[index]].item.d : 'a'}</Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  enunciado: {
+    flex: 1,
+    borderWidth: 1,
+    alignItems: 'center'
+  },
+  item: {
+    flex: 1,
+    borderWidth: 1,
+  },
+  correct: {
+    flex: 1,
+    borderWidth: 1,
+    backgroundColor: 'green',
+  }
+});
